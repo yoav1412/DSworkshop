db880dc4706e83753ae6df28edb34c62d2bd5d5b
Yoav <yoav1412@gmail.com>
2018-04-28 22:15:58 +0300

    some fixes + join with users data

--- createFeatures.py
+++ createFeatures.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-df = pd.read_csv(r"C:\Users\yoav1\OneDrive\Desktop\total.csv")
-
+raw_tappy_data = pd.read_csv(r"C:\Users\yoav1\PycharmProjects\Parkinsons\Data\our_taps.csv")
+users_data = pd.read_csv(r"C:\Users\yoav1\PycharmProjects\Parkinsons\Data\USERS.csv")
 
 def create_summary_statistics(full_data,columns_to_aggregate, aggregation_functions):
     """
@@ -19,23 +20,47 @@ def create_summary_statistics(full_data,columns_to_aggregate, aggregation_functi
         agg(aggregation_functions).reset_index()
     leftHandStats = left_right_stats[left_right_stats.Hand == 'L']
     rightHandStats = left_right_stats[left_right_stats.Hand == 'R']
-    leftHandStats.columns = ["L_"+col+"_"+stat if col !="ID" else col for col,stat in leftHandStats.columns]
-    rightHandStats.columns = ["R_"+col+"_"+stat if col !="ID" else col for col,stat in rightHandStats.columns]
+    leftHandStats.columns = ["L_"+col+"_"+stat if col not in ("ID", "Hand") else col for col,stat in leftHandStats.columns]
+    rightHandStats.columns = ["R_"+col+"_"+stat if col not in ("ID", "Hand") else col for col,stat in rightHandStats.columns]
 
     # Calculate statistics for rows that match 'LR'/'RR'/'RL'/'LL' keystrokes:
     lr_rl_ll_rr_stats = full_data.groupby(["ID", "Direction"])[columns_to_aggregate]. \
         agg(aggregation_functions).reset_index()
     ll_stats = lr_rl_ll_rr_stats[lr_rl_ll_rr_stats.Direction == "LL"]
-    ll_stats.columns = ["LL_" + col + "_" + stat if col !="ID" else col for col, stat in ll_stats.columns]
+    ll_stats.columns = ["LL_" + col + "_" + stat if col not in ("ID", "Direction") else col for col, stat in ll_stats.columns]
     lr_stats = lr_rl_ll_rr_stats[lr_rl_ll_rr_stats.Direction == "LR"]
-    lr_stats.columns = ["LR_" + col + "_" + stat if col !="ID" else col for col, stat in lr_stats.columns]
+    lr_stats.columns = ["LR_" + col + "_" + stat if col not in ("ID", "Direction") else col for col, stat in lr_stats.columns]
     rl_stats = lr_rl_ll_rr_stats[lr_rl_ll_rr_stats.Direction == "RL"]
-    rl_stats.columns = ["RL_" + col + "_" + stat if col !="ID" else col for col, stat in rl_stats.columns]
+    rl_stats.columns = ["RL_" + col + "_" + stat if col not in ("ID", "Direction") else col for col, stat in rl_stats.columns]
     rr_stats = lr_rl_ll_rr_stats[lr_rl_ll_rr_stats.Direction == "RR"]
-    rr_stats.columns = ["RR_" + col + "_" + stat if col !="ID" else col for col, stat in rr_stats.columns]
+    rr_stats.columns = ["RR_" + col + "_" + stat if col not in ("ID", "Direction") else col for col, stat in rr_stats.columns]
 
     # Join all dfs together:
-    res = leftHandStats.merge(rightHandStats, on="ID").\
-        merge(ll_stats, on="ID").merge(lr_stats, on="ID").merge(rl_stats, on="ID").merge(rr_stats, on="ID")
+    res = leftHandStats.merge(rightHandStats, on="ID", how="outer").\
+        merge(ll_stats, on="ID", how="outer").merge(lr_stats, on="ID", how="outer").\
+        merge(rl_stats, on="ID", how="outer").merge(rr_stats, on="ID", how="outer")
 
+    # Check if any users were lost during the aggregation process:
+    if len(set(res.ID.values)) < len(set(full_data.ID.values)):
+        s = set(full_data.ID.values)
+        lost = [u for u in s if u not in res.ID.values]
+        print("lost {} unique IDS in the aggregation process: ".format(len(lost)), lost)
+
+    res.set_index("ID")
+    cols = set(res.columns.values.copy())
+    for col in cols:
+        if col.startswith("Hand") or col.startswith("Direction"):
+            res = res.drop(col, axis=1)
     return res
+
+
+# Create a processed dataset with desired features calculated per user:
+data = create_summary_statistics(raw_tappy_data,
+                                 columns_to_aggregate=["FlightTime","HoldTime", "LatencyTime"],
+                                 aggregation_functions=[np.mean, np.std, stats.kurtosis, stats.skew])
+# Add a feature of the mean-diff between Left and Right HoldTimes, and Between LR and RL LatencyTimes:
+data["mean_diff_L_R_HoldTime"] = abs(data.R_HoldTime_mean - data.L_HoldTime_mean) # TODO: should this really be abs?
+data["mean_diff_LR_RL_LatencyTime"] = abs(data.RL_LatencyTime_mean - data.LR_LatencyTime_mean) # TODO: should this really be abs?
+
+# Join with the Users data:
+data = data.merge(users_data, on="ID", how="left")
